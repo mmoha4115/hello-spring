57fcd6297931fbac66f16035339341f580681330
Mohamed Mohamed <mmohamed54d0qf@gmail.com>
2018-12-24 21:02:05 -0600

    added addGrade and getGradeLevel

--- src/main/java/com/mohamed/hellospring/Playground/Student.java
+++ src/main/java/com/mohamed/hellospring/Playground/Student.java
@@ -1,23 +1,29 @@
 package com.mohamed.hellospring.Playground;
 
 public class Student {
-    private  final int id;
+    private int nextStudentId = 1;
+    private   int id;
     private String name;
     private double gpa;
     private int courseCompleted;
-    private int gradeLevel;
+    private int numberOfCredits;
 
 
-    public  Student(int id, String name, double gpa, int courseCompleted,int gradeLevel){
+    public  Student(int id, String name, double gpa, int numberOfCredits){
         this.id=id;
         this.name=name;
         this.gpa=gpa;
-        this.courseCompleted=courseCompleted;
-        this.gradeLevel=gradeLevel;
+        this.numberOfCredits=numberOfCredits;
     }
 
     public  Student(int id,String name,int gpa){
-        this(id,name,gpa,0,0);
+        this(id,name,gpa,0);
+    }
+
+    public Student(String name){
+        this.name=name;
+        this.id=nextStudentId;
+        nextStudentId++;
     }
 
 
@@ -45,11 +51,28 @@ public class Student {
         this.courseCompleted = courseCompleted;
     }
 
-    public int getGradeLevel() {
-        return gradeLevel;
+    public int getNumberOfCredits() {
+        return numberOfCredits;
     }
 
-    protected void setGradeLevel(int gradeLevel) {
-        this.gradeLevel = gradeLevel;
+    protected void addGrade(double grade,int credits) {
+        gpa = (numberOfCredits * gpa +grade*credits) / (numberOfCredits + credits);
+        numberOfCredits += credits;
     }
+
+    public String getGradeLevel(){
+        if (numberOfCredits <= 29){
+            return "Freshman";
+        }else if (numberOfCredits >= 30 & numberOfCredits <= 59) {
+            return "Sophomore";
+        }else if (numberOfCredits >= 60 & numberOfCredits <= 89){
+            return "junior";
+            }else {
+            return "Senior"; }
+    }
+
+//    public void toString(){
+//
+//    }
+
 }
